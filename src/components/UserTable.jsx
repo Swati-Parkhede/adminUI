1c0bbfccc176e4b39b6de77afe102ed7aa8884e0
swati parkhede <swatiparkhede@gmail.com>
2025-02-28 20:17:56 +0530

    display users in usertable

--- src/components/UserTable.jsx
+++ src/components/UserTable.jsx
@@ -1,22 +1,46 @@
 import React from 'react'
 
+import './UserTable.css'
+import editIcon from './icons/edit.png'
+import deleteIcon from './icons/delete.png'
 const UserTable = (props) => {
 
 
 
     return (
         <>
-            {props.users.map((user) => {
 
-                return (<div key={user.id}>
-                    <span key={user.name}>{user.name}</span>
-                    <span key={user.email}>{user.email}</span>
-                    <span key={user.role}>{user.role}</span>
-                </div>
-                )
-            }
+            <table class="usertable">
+                <tr class="table-row">
+                    <th>
+                        <input type='checkbox' />
+                    </th>
+                    <th class="table-column">Name</th>
+                    <th>Email</th>
+                    <th>Roll</th>
+                    <th>Action</th>
+                </tr>
 
-            )} 
+                {props.users.map((user) => {
+
+                    return (
+                        <tr key={user.id} >
+                            <td>
+                                <input type='checkbox' />
+                            </td>
+
+                            <td class="userdata" key={user.name}>{user.name}</td>
+                            <td class="userdata" key={user.email}>{user.email}</td>
+                            <td class="userdata" key={user.role}>{user.role}</td>
+                            <td>
+                                <span><img class="actionIcons" src={editIcon} alt="edit" /></span>
+                                <span><img class="actionIcons" src={deleteIcon} alt="delete" /></span>
+                            </td>
+                        </tr>
+                    )
+                }
+                )}
+            </table>
         </>
     )
 }
